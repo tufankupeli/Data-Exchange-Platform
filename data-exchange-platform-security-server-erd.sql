2c14f22c71efe7733d2da9e0b220f86bc92dd916
Tufan KÜPELİ <34001623+tufankupeli@users.noreply.github.com>
2023-10-04 11:51:08 +0400

    Add files via upload

---
+++ data-exchange-platform-security-server-erd.sql
@@ -0,0 +1,38 @@
+ALTER TABLE `tbl_access_right` DROP FOREIGN KEY `tbl_access_right_fk0`;
+
+ALTER TABLE `tbl_access_right` DROP FOREIGN KEY `tbl_access_right_fk1`;
+
+ALTER TABLE `tbl_access_right` DROP FOREIGN KEY `tbl_access_right_fk2`;
+
+ALTER TABLE `tbl_api_key_roles` DROP FOREIGN KEY `tbl_api_key_roles_fk0`;
+
+ALTER TABLE `tbl_certificates` DROP FOREIGN KEY `tbl_certificates_fk0`;
+
+ALTER TABLE `tbl_client` DROP FOREIGN KEY `tbl_client_fk0`;
+
+ALTER TABLE `tbl_client` DROP FOREIGN KEY `tbl_client_fk1`;
+
+ALTER TABLE `tbl_group_member` DROP FOREIGN KEY `tbl_group_member_fk0`;
+
+ALTER TABLE `tbl_group_member` DROP FOREIGN KEY `tbl_group_member_fk1`;
+
+ALTER TABLE `tbl_local_group` DROP FOREIGN KEY `tbl_local_group_fk0`;
+
+ALTER TABLE `tbl_server_configuration` DROP FOREIGN KEY `tbl_server_configuration_fk0`;
+
+ALTER TABLE `tbl_services` DROP FOREIGN KEY `tbl_services_fk0`;
+
+ALTER TABLE `tbl_time_stamp_servers` DROP FOREIGN KEY `tbl_time_stamp_servers_fk0`;
+
+ALTER TABLE `tbl_ui_user_group_entity` DROP FOREIGN KEY `tbl_ui_user_group_entity_fk0`;
+
+ALTER TABLE `tbl_ui_user_group_entity` DROP FOREIGN KEY `tbl_ui_user_group_entity_fk1`;
+
+ALTER TABLE `tbl_ui_group_service_entity` DROP FOREIGN KEY `tbl_ui_group_service_entity_fk0`;
+
+ALTER TABLE `tbl_ui_group_service_entity` DROP FOREIGN KEY `tbl_ui_group_service_entity_fk1`;
+
+ALTER TABLE `tbl_service_description` DROP FOREIGN KEY `tbl_service_description_fk0`;
+
+ALTER TABLE `tbl_endpoints` DROP FOREIGN KEY `tbl_endpoints_fk0`;
+
---
+++ data-exchange-platform.sql
@@ -0,0 +1,22 @@
+ALTER TABLE `tbl_server_clients_entity` DROP FOREIGN KEY `tbl_server_clients_entity_fk0`;
+
+ALTER TABLE `tbl_server_clients_entity` DROP FOREIGN KEY `tbl_server_clients_entity_fk1`;
+
+ALTER TABLE `tbl_security_server_clients` DROP FOREIGN KEY `tbl_security_server_clients_fk0`;
+
+ALTER TABLE `tbl_security_server_clients` DROP FOREIGN KEY `tbl_security_server_clients_fk1`;
+
+ALTER TABLE `tbl_security_servers` DROP FOREIGN KEY `tbl_security_servers_fk0`;
+
+ALTER TABLE `tbl_auth_certs` DROP FOREIGN KEY `tbl_auth_certs_fk0`;
+
+ALTER TABLE `tbl_requests` DROP FOREIGN KEY `tbl_requests_fk0`;
+
+ALTER TABLE `tbl_requests` DROP FOREIGN KEY `tbl_requests_fk1`;
+
+ALTER TABLE `tbl_requests` DROP FOREIGN KEY `tbl_requests_fk2`;
+
+ALTER TABLE `tbl_user_group_members` DROP FOREIGN KEY `tbl_user_group_members_fk0`;
+
+ALTER TABLE `tbl_user_group_members` DROP FOREIGN KEY `tbl_user_group_members_fk1`;
+
